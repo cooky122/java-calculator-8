5b00dfb91f11e2b75b09f0b71c0ed7e45ae27925
test1 <cooky122@naver.com>
2025-10-18 16:24:57 +0900

    feat(Application): 프로그램 입출력 부분 구현
    Application 클래스에 Console API를 이용한 사용자 입력과 System.out을 통한 결과 출력 로직을 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("enter a string:");
+
+        // 문자열 입력
+        String inputText = Console.readLine();
+
+        try {
+            // 2. 핵심 로직은 StringCalculator 객체에 위임한다.
+            Calculator calculator = new Calculator();
+            int result = calculator.calculate(inputText); // add가 아닌 calculate로 이름 변경
+
+            // 결과 출력
+            System.out.println("result: " + result);
+
+        } catch (Exception e) {
+            // 5. 예외를 처리한다.
+            e.printStackTrace();
+        }
     }
 }
