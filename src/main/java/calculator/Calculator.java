23f3e562ad07e7008e6bb7ea5777a0c282b2d3eb
test1 <cooky122@naver.com>
2025-10-18 17:14:47 +0900

    feat(Calculator): 커스텀 구분자 없는 경우 숫자 계산
    "," 구분자로 숫자 분리해 숫자 합을 반환하는 메소드를 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,9 +3,27 @@ package calculator;
 public class Calculator {
     public int calculate(String inputText) {
         if (!inputText.contains("\\n")) {
-            return 0;
+            String numberWithDelimiter = inputText.replace(":", ",");
+
+            return sumNumbers(numberWithDelimiter);
         } else {
             return 0;
         }
+    }
+
+    private int sumNumbers(String numberWithDelimiter) {
+        int sum = 0;
+
+        String[] numbers = numberWithDelimiter.split(",");
+        for (String number : numbers) {
+            try {
+                if (!number.isEmpty()) {
+                    sum += Integer.parseInt(number);
+                }
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("input error");
+            }
+        }
+        return sum;
     }
 }
