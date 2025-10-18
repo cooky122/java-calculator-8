ee261e94db8557c76ed3d63530800feafdf468c2
test1 <cooky122@naver.com>
2025-10-18 17:25:52 +0900

    feat(Calculator): 커스텀 구분자 있는 경우 숫자 계산
    커스텀 구분자를 확인해 "," 구분자로 변환 후 문자열을 반환하는 메소드 추가
    메소드 호출해 합 계산

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,7 +7,9 @@ public class Calculator {
 
             return sumNumbers(numberWithDelimiter);
         } else {
-            return 0;
+            String numberWithDelimiter = getStringWithCustomDelimiter(inputText);
+
+            return sumNumbers(numberWithDelimiter);
         }
     }
 
@@ -25,5 +27,16 @@ public class Calculator {
             }
         }
         return sum;
+    }
+
+    private String getStringWithCustomDelimiter(String inputText) {
+        if (inputText.indexOf("\\\\n") != inputText.lastIndexOf("\\\\n")) {
+            throw new IllegalArgumentException("input error");
+        }
+        String[] lines = inputText.split("\\\\n");
+        String seperator = lines[0].replace("//", "");
+        String numbers = lines[1];
+
+        return numbers.replace(":", ",").replace(seperator, ",");
     }
 }
