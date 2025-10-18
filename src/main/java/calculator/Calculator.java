8adc81e70e80c30bfdf43cb42ff97e0c1fdb9e39
test1 <cooky122@naver.com>
2025-10-18 17:06:24 +0900

    feat(Calculator): 커스텀 구분자 확인
    커스텀 구분자 입력을 구분하는 "\n" 유무 확인 if문 추가

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+    public int calculate(String inputText) {
+        if (!inputText.contains("\\n")) {
+            return 0;
+        } else {
+            return 0;
+        }
+    }
+}
